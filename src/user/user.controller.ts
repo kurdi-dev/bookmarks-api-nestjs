bf0e262209ff7704d5eb27c8224e4764a74ccedf
Walid <walid.r.rashed@gmail.com>
2022-02-25 01:00:37 +0300

    chore: using custom getUser decorator in user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
 
-import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
 import { JWTGuard } from 'src/auth/guard';
 
 @UseGuards(JWTGuard)
 @Controller('users')
 export class UserController {
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
