376cb37b4036e8d064fe639ff1eae30e9a602db2
Walid <walid.r.rashed@gmail.com>
2022-02-28 02:23:09 +0300

    chore: bookmarks dto files created

---
+++ src/bookmark/dto/edit-bookmark.dto.ts
@@ -0,0 +1,13 @@
+import { IsOptional, IsString } from 'class-validator';
+
+export class EditBookmarkDto {
+  @IsString()
+  @IsOptional()
+  title?: string;
+  @IsString()
+  @IsOptional()
+  link?: string;
+  @IsString()
+  @IsOptional()
+  description?: string;
+}
