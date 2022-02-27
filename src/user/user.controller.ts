2059d343f3c5e2438280f22cbc48e148fee30755
Walid <walid.r.rashed@gmail.com>
2022-02-28 02:22:38 +0300

    feat: user edit endpoint created

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 
 import { GetUser } from '../auth/decorator';
 import { JWTGuard } from '../auth/guard';
+import { UserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JWTGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @Patch('edit')
+  editUser(@GetUser('id') userId: number, @Body() dto: UserDto) {
+    return this.userService.editUser(userId, dto);
   }
 }
